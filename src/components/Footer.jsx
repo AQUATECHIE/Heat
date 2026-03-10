8b8c1a75aa04323274cda9aa1d55d660c700d9ba
AQUATECHIE <amolequwam96@gmail.com>
2026-03-11 00:15:49 +0100

    auto-commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,73 @@
+import { useState } from "react";
 import "../styles/Footer.css";
 import { FaArrowRight, FaEnvelope, FaPhone, FaInstagram } from "react-icons/fa";
+import api from "../api/axios";
 
 const Footer = () => {
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+ const handleSubscribe = async () => {
+
+  if (!email) {
+    setMessage("Please enter email");
+    return;
+  }
+
+  try {
+
+    const { data } = await api.post("/newsletter", {
+      email
+    });
+
+    setMessage(data.message);
+    setEmail("");
+
+  } catch (error) {
+
+    console.log(error);
+
+    setMessage(
+      error.response?.data?.message || "Subscription failed"
+    );
+
+  }
+};
+
   return (
     <footer className="footer">
-
+   
       {/* Newsletter */}
       <div className="newsletter">
+
         <h3>SIGN UP FOR HEAT ONLY UPDATES</h3>
+
         <p>
           By entering your email address below, you consent to receiving our
           newsletter with access to our latest collections, events and initiatives.
         </p>
 
         <div className="newsletter-input">
-          <input type="email" placeholder="Email" />
-          <FaArrowRight className="arrow-icon" />
+
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+
+          <FaArrowRight
+            className="arrow-icon"
+            onClick={handleSubscribe}
+          />
+
         </div>
+
+        {message && (
+          <p className="newsletter-message">{message}</p>
+        )}
+
       </div>
 
       {/* Contact */}
